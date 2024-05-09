a6c5cba30722fedcdd53e3648ee29251925be797
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2024-05-09 13:31:23 +0530

    chg

--- source.py
+++ source.py
@@ -1,29 +1,120 @@
+from pathlib import Path
+
 import streamlit as st
+from PIL import Image
 
 
+# --- PATH SETTINGS ---
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+resume_file = current_dir / "assets" / "CV.pdf"
+profile_pic = current_dir / "assets" / "profile-pic.png"
 
-# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
-lottie_coding = "https://app.lottiefiles.com/animation/272fb5b7-a1fb-4e24-aeb6-ce94eec51b6b"
-with st.container():
-    st.subheader("Hi, I am Pradeep  :wave:")
-    st.title("A Software engineer from India")
-    st.write("i am passionate about finding ways to use python and VBA to be moreeffective")
-    st.write("[Learn More >](https://pythonandvba.com)")
+# --- GENERAL SETTINGS ---
+PAGE_TITLE = "Digital CV | Pradeep Yadav"
+PAGE_ICON = ":wave:"
+NAME = "Pradeep Yadav"
+DESCRIPTION = """
+Software Developer.
+"""
+EMAIL = "yadavpradeep2313@gmail.com"
 
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.header("What I do")
-        st.write("##")
-        st.write(
-            """
-             On my YouTube channel I am creating tutorials for people who:
-            - are looking for a way to leverage the power of Python in their day-to-day work.
-            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
-            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
-            - are working with Excel and found themselves thinking - "there has to be a better way.
-            """
-        )
+# --- Social Links ---
+SOCIAL_MEDIA = {
+    "LinkedIn": "https://www.linkedin.com/in/pradeep-yadav-5aa47a20b/",
+    "GitHub": "https://github.com/Pradeep9651",
+    "leetcode":"https://leetcode.com/u/yadavpradeep2313/"
+}
+PROJECTS = {
+    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
+    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
+    "🏆 Brain Tumor Detection System-Web app using Python & Streamlit":"https://github.com/Pradeep9651/Brain_tumor"
+}
+
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+
+# --- LOAD CSS, PDF & PROFIL PIC ---
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+profile_pic = Image.open(profile_pic)
+
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.image(profile_pic, width=230)
+
+with col2:
+    st.title(NAME)
+    st.write(DESCRIPTION)
+    st.download_button(
+        label=" 📄 Download Resume",
+        data=PDFbyte,
+        file_name=resume_file.name,
+        mime="application/octet-stream",
+    )
+    st.write("📫", EMAIL)
+
+
+# --- SOCIAL LINKS ---
+st.write("#")
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
+
+
+# --- Education & QUALIFICATIONS ---
+st.write("#")
+st.subheader("Education & QUALIFICATIONS")
+st.write(
+    """
+- ✔️ Bachelor of Information Technology., United Institute Of Technology Expected 2024
+- ✔️ High School (X) , Kendriya Vidyalaya, CBSE Board . 2018
+- ✔️Intermediate (XII) , Ramanujan Public School, CBSE Board . 2020
+
+"""
+)
+
+
+# --- SKILLS ---
+st.write("#")
+st.subheader("Hard Skills")
+st.write(
+    """
+- 👩‍💻 Programming: Python, SQL, VBA, C++
+- 📊 Data Visulization: PowerBi, MS Excel, Plotly
+- 📚 Modeling: Logistic regression, linear regression, decition trees
+- 🗄️ Databases: MongoDB, MySQL
+"""
+)
+
+
+# --- TRAINING/ CERTIFICATION COURSES ---
+st.write("#")
+st.subheader("TRAINING/ CERTIFICATION COURSES")
+st.write("---")
+
+# --- Certification 1
+st.write("🚧", "**Python Programming**")
+
+
+# --- Certification 2
+st.write("#")
+st.write("🚧", "**Data Analytics Virtual experience**")
+
+
+# --- Certification 3
+st.write("#")
+st.write("🚧", "**Full stack development**")
+
+# --- Projects & Accomplishments ---
+st.write("#")
+st.subheader("Projects & Accomplishments")
+st.write("---")
+for project, link in PROJECTS.items():
+    st.write(f"[{project}]({link})")
