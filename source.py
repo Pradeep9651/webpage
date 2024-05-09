fae472638c2a084fda31183c4ec136aaf113209f
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2024-05-09 13:56:48 +0530

    firse

--- source.py
+++ source.py
@@ -1,17 +1,9 @@
 import streamlit as st
 
 
+
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
-
-lottie_coding = "https://app.lottiefiles.com/animation/272fb5b7-a1fb-4e24-aeb6-ce94eec51b6b"
-
-# --- PATH SETTINGS ---
-current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
-profile_pic = current_dir / "assets" / "profile-pic.png"
-
 with st.container():
     st.subheader("Hi, I am Pradeep  :wave:")
     st.title("A Software engineer from India")
